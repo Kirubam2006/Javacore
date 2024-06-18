dbc2ca09cba3da9cff8e59778d9cd68717c28be7
Dell <Dell@192.168.1.40>
2024-06-18 17:21:43 +0530

    first java codes

---
+++ day1/src/day11/IfDemo.java
@@ -0,0 +1,14 @@
+package day11;
+
+public class IfDemo {
+
+	public static void main(String[] args) {
+		int x=5;
+		if(x!=5);
+		System.out.println(+x);
+		
+		
+
+	}
+
+}
---
+++ day1/src/day11/welcome.java
@@ -0,0 +1,11 @@
+package day11;
+
+public class welcome {
+
+	public welcome() {
+		welcome welcome=new welcome();
+		System.out.println("WELCOME:");
+		
+	}
+
+}
